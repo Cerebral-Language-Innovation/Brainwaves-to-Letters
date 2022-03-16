e3d4f3dba480078995cc9bd54135255cc22028b6
Leo Sandler <leo.sandler@queensu.ca>
2022-03-16 18:19:50 -0400

    Changed collect_sample.py to account for bad recordings and modify file names.

--- protocol_functions/collect_sample.py
+++ protocol_functions/collect_sample.py
@@ -5,6 +5,17 @@ import time
 from pylsl import *
 
 
+def keep():
+    bad_recording = str(input('Keep this recording? \n Enter (Y\\N): '))
+    if bad_recording.upper() == "Y":
+        return True
+    elif bad_recording.upper() == "N":
+        return False
+    else:
+        print("Invalid input. Enter Y or N.")
+        return keep()
+
+
 def run(sample_length, action_name, file_name):
     stream = resolve_stream('type', 'EEG')
     inlet = StreamInlet(stream[0])
@@ -20,11 +31,11 @@ def run(sample_length, action_name, file_name):
 
     # creating a data set
     c1, c2, c3, c4, timestamps = [], [], [], [], []
-    names = ['time', 'c1', 'c2', 'c3', 'c4'] # corrected names for the channel columns
+    names = ['time', 'c1', 'c2', 'c3', 'c4']  # corrected names for the channel columns
     df = pd.DataFrame(columns=names)
     start = time.time()
     # Starts a GUI to notify the participant when to start their action.
-    protocol_functions.tkinter_timer.run(sample_length, action_name.title(), int(sample_length/2))
+    protocol_functions.tkinter_timer.run(sample_length, action_name.title(), int(sample_length / 2))
     while 1:
         s, t = inlet.pull_sample()
         print(s, t)
@@ -37,5 +48,7 @@ def run(sample_length, action_name, file_name):
             ignore_index=True)
         if time.time() - start > sample_length:
             break
-
-    return df.to_csv(file_name) # converting data to csv
+    keep_file = keep()
+    if not keep_file:
+        file_name = file_name[:-4] + '_FATAL' + file_name[-4:]  # Adds FATAL to the file name without changing the CSV type
+    return df.to_csv(file_name)  # converting data to csv
