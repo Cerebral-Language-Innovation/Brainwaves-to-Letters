ae3112853e0312934173171995135c676780cdf3
Leo Sandler <leo.sandler@queensu.ca>
2022-03-16 17:32:52 -0400

    Made csv-to-pandas more modular so it can be called in other files.

--- csv-to-pandas.py
+++ csv-to-pandas.py
@@ -1,42 +1,51 @@
 import os
 import glob
 import pandas as pd
-from IPython.core.display import display
 
 # script to convert all csv files into one pandas Dataframe. Note the read in is not in the order stored on the computer.
-# displaying/printing is odd due to the large number of entries per "index". use .iloc[] to access a specific "file"
 # .iloc[][] will return that specific row and column in the data frame respectively.
-# I called the data frame ye cuz idk why not
-
-# Path to files where all csv data folders are stored
-path_to_files = "/Users/markbenhamu/Desktop/QCLI/Data/Brainwaves-to-Letters/Bite and Blink Data Analysis v1"
-os.chdir(path_to_files)
-file_extension = '.csv'
-all_filenames = [i for i in glob.glob(f"*{file_extension}")]
-
-# These are the column headers. Action type, time logs then all 4 channels (c1->c4) in order
-tmp = [['Type', 'Time', 'c1', 'c2', 'c3', 'c4']]
 
 # "Movement" types.. change as needed
-types = ['baseline', 'bite', 'blink']
+MOVEMENT_TYPES = ['baseline', 'bite', 'blink']
 
+FILEPATH = path_to_files = "/Users/markbenhamu/Desktop/QCLI/Data/Brainwaves-to-Letters/Bite and Blink Data Analysis v1"
 
-# Determines action type from what was saved in file. Should be modular enough for any changes in what actions we are recording
+# TODO: Make file path/naming more modular
+'''
+Function looks for the path to the data directory
+'''
+def dynamic_filepath():
+    for root, dirs, files in os.walk(os.getcwd(), topdown=False):
+        for name in dirs:
+            if name == "Bite and Blink Data Analysis v1":
+                return os.path.join(root, name)
+
+def csv_to_dataframe(path_to_files = None):
+    if path_to_files is not None:
+        os.chdir(path_to_files)
+    else:
+        os.chdir(dynamic_filepath())
+
+    file_extension = '.csv'
+
+    # These are the column headers. Action type, time logs then all 4 channels (c1->c4) in order
+    tmp = [['Type', 'Time', 'c1', 'c2', 'c3', 'c4']]
+
+    for i in glob.glob(f"*{file_extension}"):
+        type = check_type(i)
+        data = pd.read_csv(i, usecols=[1, 6, 7, 8, 9])  # based on current csv formatting
+        tmp.append([type, data['time'], data['c1'], data['c2'], data['c3'], data['c4']])
+
+    headings = tmp.pop(0)
+    temp = pd.DataFrame(tmp, columns=headings)
+    return temp
+
+'''
+Determines action type from CSV name.
+@:returns movement type name if found in file name
+'''
 def check_type(file_name):
-    a = 0
-    while a < len(types):
-        if types[a] in file_name.lower():
-            return types[a]
-        a = a + 1
-
-
-for i in glob.glob(f"*{file_extension}"):
-    Type = check_type(i)
-    data = pd.read_csv(i, usecols=[1, 6, 7, 8, 9])  # based on current csv formatting
-    tmp.append([Type, data['time'], data['c1'], data['c2'], data['c3'], data['c4']])
-
-headings = tmp.pop(0)
-ye = pd.DataFrame(tmp, columns=headings)
-display(ye.columns.values)
-print(ye.iloc[0])
-print(ye.iloc[len(ye)-1])
+    for a in range(len(MOVEMENT_TYPES)):
+        if MOVEMENT_TYPES[a] in file_name.lower():
+            return MOVEMENT_TYPES[a]
+    raise ValueError("Input file: " + file_name + " has no recognizable movement type")
