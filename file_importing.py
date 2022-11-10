4fbe9bd103cd301c479908652d23795dafbec9eb
Leo Sandler <leo.sandler@queensu.ca>
2022-11-10 15:14:39 -0500

    Added file importing code with functions for searching through files

---
+++ file_importing.py
@@ -0,0 +1,28 @@
+import glob
+
+def get_newer_files():
+    path = 'Bite and Blink Data Analysis v1'
+    bites = []
+    blinks = []
+    files = glob.glob(path + '/*.csv')
+    for f in files:
+        if 'blink' in f:
+            blinks.append(f)
+        elif 'bite' in f:
+            bites.append(f)
+    return bites, blinks
+
+def get_all_files():
+    path = 'Bite and Blink Data Analysis v1'
+    c1 = []
+    c2 = []
+    c3 = []
+    files = glob.glob(path + "/*.csv")
+    for f in files:
+        if 'link_' in f or 'link.' in f:
+            c1.append(f)
+        elif 'ite_' in f or 'ite.' in f:
+            c2.append(f)
+        elif 'aseline_' in f or 'aseline.' in f:
+            c3.append(f)
+    return c1, c2, c3
